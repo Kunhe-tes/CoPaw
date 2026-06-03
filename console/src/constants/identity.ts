9fd2d3ffe90d712747a56f41e598ce89342e1b0c
shixiangyi <yishixiang@gmail.com>
2026-06-03 14:37:59 +0800

    chore(console): default source id to RMASSIST

--- console/src/constants/identity.ts
+++ console/src/constants/identity.ts
@@ -25,7 +25,7 @@ export const DEFAULT_TENANT_ID = "default";
 export const DEFAULT_CHANNEL = "console";
 
 /** 默认 Source ID（非 iframe 模式时作为 X-Source-Id 头发送） */
-export const DEFAULT_SOURCE_ID = "default";
+export const DEFAULT_SOURCE_ID = "RMASSIST";
 
 /** 默认 BBK ID （非iframe模式时，X-Bbk-Id header使用，空表示不传） */
 export const DEFAULT_BBK_ID = "";
