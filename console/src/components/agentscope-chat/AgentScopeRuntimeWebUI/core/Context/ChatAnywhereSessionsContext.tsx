7f787cb61170e1cd2862fdffe54df95b26a06313
Kun He <554538186@qq.com>
2026-07-28 18:41:46 +0800

    :rewind: [revert] revert changes

--- console/src/components/agentscope-chat/AgentScopeRuntimeWebUI/core/Context/ChatAnywhereSessionsContext.tsx
+++ console/src/components/agentscope-chat/AgentScopeRuntimeWebUI/core/Context/ChatAnywhereSessionsContext.tsx
@@ -15,7 +15,6 @@ import { useAsyncEffect } from "ahooks";
 import useChatAnywhereEventEmitter, {
   emit,
 } from "./useChatAnywhereEventEmitter";
-import { shouldShowContentOnlySessionNotFound } from "./contentOnlySessionError";
 import { getInitialSessionId } from "@/pages/Chat/sessionApi/initialSessionSelection";
 import { shouldApplySessionLoadResult } from "@/pages/Chat/sessionApi/sessionRaceGuard";
 
@@ -303,9 +302,7 @@ export function ChatAnywhereSessionsContextProvider(props: {
 /**
  * 会话切换时加载消息和判断重连的 hook，必须保证只挂载一次
  */
-export const useChatAnywhereSessionLoader = (
-  showContentOnlyNotFound = false,
-) => {
+export const useChatAnywhereSessionLoader = () => {
   const currentSessionId = useContextSelector(
     ChatAnywhereSessionsContext,
     (v) => v.currentSessionId,
@@ -323,40 +320,17 @@ export const useChatAnywhereSessionLoader = (
     ChatAnywhereSessionsContext,
     (v) => v.setSessionLoading,
   );
-  const [notFoundSessionId, setNotFoundSessionId] = React.useState<
-    string | undefined
-  >();
 
   useAsyncEffect(async () => {
-    setNotFoundSessionId(undefined);
-
-    try {
-      await loadSessionMessages({
-        requestedSessionId: currentSessionId,
-        clearBeforeLoad: true,
-        options,
-        setMessages,
-        getCurrentSessionId,
-        setSessionLoading,
-      });
-    } catch (error) {
-      if (
-        shouldShowContentOnlySessionNotFound({
-          enabled: showContentOnlyNotFound,
-          error,
-          requestedSessionId: currentSessionId,
-          currentSessionId: getCurrentSessionId(),
-        })
-      ) {
-        setNotFoundSessionId(currentSessionId);
-        return;
-      }
-
-      throw error;
-    }
-  }, [currentSessionId, showContentOnlyNotFound]);
-
-  return Boolean(currentSessionId && notFoundSessionId === currentSessionId);
+    await loadSessionMessages({
+      requestedSessionId: currentSessionId,
+      clearBeforeLoad: true,
+      options,
+      setMessages,
+      getCurrentSessionId,
+      setSessionLoading,
+    });
+  }, [currentSessionId]);
 };
 
 /**
