5a6cd67365bba4f11d072f7c162b9c3a569fc7bd
Kun He <554538186@qq.com>
2026-08-05 14:05:23 +0800

    :bug: (fix) restore quick navigation overlay layout

--- console/src/pages/Chat/index.tsx
+++ console/src/pages/Chat/index.tsx
@@ -1875,24 +1875,18 @@ export default function ChatPage() {
                 onDragOver={isContentOnly ? undefined : handleDragOver}
                 onDrop={isContentOnly ? undefined : handleDrop}
               >
-                <div className={styles.chatMessagesLayout}>
-                  <div className={styles.chatMessagesViewport}>
-                    <ChatContentOnlyProvider enabled={isContentOnly}>
-                      <GlobalVoiceRecorder enabled={voiceRecorderEnabled}>
-                        <AgentScopeRuntimeWebUILayout ref={chatRef} />
-                      </GlobalVoiceRecorder>
-                    </ChatContentOnlyProvider>
-                  </div>
-                  <div className={styles.chatQuickNavRail}>
-                    <ConversationQuickNav placement="rail" />
-                  </div>
-                </div>
+                <ChatContentOnlyProvider enabled={isContentOnly}>
+                  <GlobalVoiceRecorder enabled={voiceRecorderEnabled}>
+                    <AgentScopeRuntimeWebUILayout ref={chatRef} />
+                  </GlobalVoiceRecorder>
+                </ChatContentOnlyProvider>
                 {!isContentOnly && (
                   <DragUploadOverlay
                     visible={isDragging}
                     onClose={handleDragOverlayClose}
                   />
                 )}
+                <ConversationQuickNav />
               </div>
             </div>
           </AutoPreviewHtmlProvider>
